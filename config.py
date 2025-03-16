a82f65ec2c73bc1930c5bb2b706f08630137b3c4
tantinoroj <tan.t@chevron.com>
2025-03-16 10:52:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    # AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/f958e84a-92b8-439f-a62d-4f45996b6d07"
 
     CLIENT_ID = 'e2b17eb9-7334-4c3a-ba90-bbd09b4aa1d4'
 
