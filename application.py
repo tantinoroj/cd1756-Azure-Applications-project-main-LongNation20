6fbe93317061303cf83259a7c8714a699a54049f
tantinoroj <tan.t@chevron.com>
2025-05-07 14:07:30 +0700

    Update application.py

--- application.py
+++ application.py
@@ -11,9 +11,9 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '5000'))
     except ValueError:
-        PORT = 5555
+        PORT = 5000
     app.run(HOST, PORT, ssl_context='adhoc')
 
 # if __name__ == '__main__':
