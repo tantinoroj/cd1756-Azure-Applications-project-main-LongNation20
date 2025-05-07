2297c0679a6713ed1fa660ddb9779775e175a336
tantinoroj <tan.t@chevron.com>
2025-05-07 21:34:43 +0700

    Update application.py

--- application.py
+++ application.py
@@ -15,20 +15,3 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5000
     app.run(HOST, PORT, ssl_context='adhoc')
-
-# if __name__ == '__main__':
-
-#     # Use Azure Web App settings if available, otherwise use local dev settings
-#     # HOST = environ.get('WEBSITE_HOSTNAME', 'localhost')
-#     try:
-#         PORT = int(environ.get('WEBSITE_PORT', '8000'))
-#         # port = int(os.environ.get('PORT', 8000))
-#         app.run(host='0.0.0.0', port=port)
-#     except ValueError:
-#         PORT = 8000
-        
-#     # Use ssl_context only in local development
-#     if HOST == 'localhost':
-#         app.run(HOST, PORT, ssl_context='adhoc')
-#     else:
-#         app.run(HOST, PORT)
